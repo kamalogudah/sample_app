2785043998dbbd71e2fceffc506cf0f7840cc295
Paul Oguda <mcpaul2058@gmail.com>
2014-08-14 01:08:13 +0300

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ describe User do
  it { should respond_to(:password_digest) }
  it { should respond_to(:password) } 
  it { should respond_to(:password_confirmation) }
+ it { should respond_to(:remember_token) }
  it { should be_valid }
  it { should respond_to(:authenticate) }
  
@@ -80,6 +81,14 @@ describe User do
    it { should_not eq user_for_invalid_password }
    specify { expect(user_for_invalid_password).to be_false }
    end
- end
+   describe "remember token" do
+     before { @user.save }
+       its(:remember_token) { should_not be_blank }
+     end
+   end
 end
+
+	
+ 
+
 
